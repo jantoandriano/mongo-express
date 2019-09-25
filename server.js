8fd6e7af26edff3dc9e8440da4d543be5f2c76af
Janto Andriano <motulojanto@gmail.com>
2019-09-25 15:06:16 +0700

    update server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const cors = require("cors")
-const db = require("./config/mongodb__config")
+const db = require("./config/database")
 const {PORT} = require("./config/variableEnv")
 
 const bodyParser = require("body-parser")
